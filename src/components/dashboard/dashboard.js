472b27433365f6e3a2bde6e38b00fb5e211bbc18
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2023-05-31 23:56:37 +0530

    dashboard

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,10 +1,36 @@
 import "./dashboard.css";
 import profile from "./assets/passport.jpg";
 import Navbar from "../navbar/navbar";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function Dashboard() {
   let today = new Date().toLocaleDateString();
   console.log(today);
+  const [totalSubmissions, setTotalSubmissions] = useState(0);
+  const [reviewedCandidates, setReviewedCandidates] = useState(0);
+
+  useEffect(() => {
+    // Fetch total submissions count
+    axios
+      .get("/api/hr/getshortlistedcount")
+      .then((response) => {
+        setTotalSubmissions(response.data.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    // Fetch reviewed candidates count
+    axios
+      .get("/api/hr/getreviewedcount")
+      .then((response) => {
+        setReviewedCandidates(response.data.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
   return (
     <div className="container">
       <Navbar />
@@ -25,13 +51,15 @@ function Dashboard() {
         <div className="dashboard-hero">
           <div className="metric metric-one">
             <p>Total Submissions</p>
+            <p>{totalSubmissions}</p>
           </div>
           <div className="metric metric-two">
             <p>Reviewed Candiates </p>
+            <p>{reviewedCandidates}</p>
           </div>
-          <div className="metric metric-three">
+          {/* <div className="metric metric-three">
             <p>Hired Candidates</p>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
